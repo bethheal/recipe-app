18ed043dc6fdce47eb2371dea3f2ea679de68f3a
elizabeth-barden <bethheal9@gmail.com>
2024-06-27 09:28:38 +0000

    React Componets

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+function NavBar(){
+    return(
+        <>
+        <nav className="flex justify-between bg-red-600">
+        <a className="navLinks  hover:bg-sky-700" href="">Home</a>
+        <a className="navLinks  hover:bg-sky-700" href="">About</a>
+        <a className="navLinks  hover:bg-sky-700" href="">Contact Us</a>
+        </nav>
+
+        </>
+    )
+}
+export default NavBar;
